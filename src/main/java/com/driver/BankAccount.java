277a9d3749cc481769b0d14bec2d2157e62bb5b5
shubham_hatkar <shubhamhatkar9765@gmail.com>
2023-05-19 17:37:15 +0530

    Banking Assignment added

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -6,26 +6,79 @@ public class BankAccount {
     private double balance;
     private double minBalance;
 
-    public BankAccount(String name, double balance, double minBalance) {
+    public BankAccount(String name, double balance, double minBalance)
+    {
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
+    }
 
+    public BankAccount(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
+        if(digits * 9 < sum)
+            throw new Exception("Account Number can not be generated");
 
-        return null;
+        String accountNumber = "";
+        while(sum > 9)
+        {
+            accountNumber += '9';
+            sum -= 9;
+        }
+        accountNumber += (sum + "");
+        while (accountNumber.length() < digits)
+        {
+            accountNumber += '0';
+        }
+        return accountNumber;
     }
 
     public void deposit(double amount) {
         //add amount to balance
-
+        this.balance += amount;
+        System.out.println("Amount " + amount + " is added to your account");
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+        if(this.balance - amount >= minBalance)
+        {
+            balance -= amount;
+            System.out.println("Amount " + amount + " is withdrawal from your account");
+        }
+        else
+        {
+            throw new RuntimeException("Insufficient Balance");
+        }
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
+    }
 }
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,11 +1,17 @@
 package com.driver;
 
+import java.util.Arrays;
+
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
-    public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
+    public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception
+    {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-
+        super(name, balance, 5000);
+        this.tradeLicenseId = tradeLicenseId;
+        if(balance < 5000)
+            throw new Exception("Insufficient Balance");
     }
 
     public void validateLicenseId() throws Exception {
@@ -13,7 +19,85 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        if(isTwoConsecutive(tradeLicenseId))
+        {
+            String reArrange = arrangeId(tradeLicenseId);
+            if(reArrange.equals("-1"))
+            {
+                throw new Exception("Valid License can not be generated");
+            }
+            else this.tradeLicenseId = reArrange;
+        }
+    }
 
+    private String arrangeId(String tradeLicenseId)
+    {
+        int n = tradeLicenseId.length();
+
+        int freq[] = new int[26];
+        for(int i = 0; i < n; i++)
+        {
+            char ch = tradeLicenseId.charAt(i);
+            freq[ch - 'A']++; // capital characters
+        }
+
+        char charWithMaxCount = getMaxCountChar(freq);
+        int maxCount = freq[charWithMaxCount - 'A'];
+
+        if(maxCount > (n + 1) / 2)
+        {
+            return "";
+        }
+
+        char arr[] = new char[n];
+        Arrays.fill(arr,'1');
+        int idx = 0;
+        while(maxCount > 0)
+        {
+            arr[idx] = charWithMaxCount;
+            idx += 2;
+            maxCount--;
+        }
+        freq[charWithMaxCount] = 0;
+        for(int i = 0; i < 26; i++)
+        {
+            while(freq[i] > 0)
+            {
+                idx = (idx >= n) ? 1 : idx;
+                arr[i] = (char)('A' + i);
+                idx += 2;
+                freq[i]--;
+            }
+        }
+        return new String(arr);
+
+    }
+
+    // find char with max freq.
+    private char getMaxCountChar(int[] freq)
+    {
+        int max = 0;
+        char ch = 'A';
+        for(int i = 0; i < 26; i++)
+        {
+            if(max > freq[i])
+            {
+                max = freq[i];
+                ch = (char)('A' + i);
+            }
+        }
+        return ch;
+    }
+
+
+    private boolean isTwoConsecutive(String tradeLicenseId)
+    {
+        int n = tradeLicenseId.length();
+        for(int i = 0; i < n - 1; i++)
+        {
+            if(tradeLicenseId.charAt(i) != tradeLicenseId.charAt(i+1)) return true;
+        }
+        return false;
     }
 
 }
